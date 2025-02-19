7bdcaa72bb43395dca7a33f3360403c5d77cf38b
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-02-19 15:17:22 +0900

    fillableを設定、不正なデータアクセスを防ぐため

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $table = 'events';
+
+    protected $fillable = [
+        'date',
+        'venue',
+        'title',
+        'open',
+        'start',
+        'ticket_price',
+        'stream_ticket_price',
+        'stream_url',
+        'status',
+    ];
 }
